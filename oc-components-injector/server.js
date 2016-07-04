298cd0734959a8aef5ea3fc94e74944cfed03613
mattia richetto <mattia.richetto@gmail.com>
2016-07-04 23:25:41 +0100

    injector w/ server side components rendering

--- oc-components-injector/server.js
+++ oc-components-injector/server.js
@@ -1,6 +1,32 @@
+var Client = require('oc-client');
+
 module.exports.data = function (context, callback) {
   'use strict';
   var pkg = require('./package');
   var model = pkg;
-  callback(null, model);
+
+  var client = new Client({
+    registries: { serverRendering: 'http://localhost:3000/' }
+  });
+
+  client.renderComponents(
+    require('./components'),
+    {
+      container: false,
+      headers: {
+        'accept-language': 'en-GB'
+      },
+      timeout: 3.0
+    }, function (errors, htmls) {
+      if (errors)
+        console.log('errors:', errors);
+      console.log('htmls:', htmls);
+      console.log('htmls.length:', htmls.length);
+
+
+      var experiments = htmls;
+      model.experiments = htmls;
+
+      callback(null, model);
+    });
 };
--- web-app/routes/index.js
+++ web-app/routes/index.js
@@ -4,20 +4,12 @@ var Client = require('oc-client');
 
 router.get('/', function (req, res, next) {
   var client = new Client({
-    registries: { serverRendering: 'http://localhost:3000/' },
-    components: {
-      'oc-components-injector': '0.1.X',
-      'oc-omg-exp-001': '0.1.X'
-    }
+    registries: { serverRendering: 'http://localhost:3000/' }
   });
 
   var components = [
     {
       name: 'oc-components-injector',
-      version: '0.1.X'
-    },
-    {
-      name: 'oc-omg-exp-001',
       version: '0.1.X'
     }
   ];
@@ -35,9 +27,8 @@ router.get('/', function (req, res, next) {
         console.log('errors:', errors);
       console.log('htmls:', htmls);
 
-      var injector = htmls.splice(0, 1);
-      var experiments = htmls;
-      var model = { title: 'small-lama/web-app', injector: injector, experiments: experiments };
+      var injector = htmls;
+      var model = { title: 'small-lama/web-app', injector: injector };
       return res.render('index', model);
     });
 });
